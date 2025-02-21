4e16b0ef691309da4a5e33b81a75f0eb07862573
Maria <mariagracielaesquivelfernandez@gmail.com>
2025-02-21 10:37:47 -0300

    Update banner component images

--- components/banners.tsx
+++ components/banners.tsx
@@ -9,18 +9,23 @@ export default function Banners() {
      autoplay
      autoplayDelay={10000}>
       <img
-        src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
+        src="img-slider-1.png"
         alt="image 1"
         className="h-full w-full object-cover"
       />
       <img
-        src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
+        src="img-slider-2.png"
         alt="image 2"
         className="h-full w-full object-cover"
       />
       <img
-        src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
+        src="img-slider-3.png"
         alt="image 3"
+        className="h-full w-full object-cover"
+      />
+       <img
+        src="img-slider-4.png"
+        alt="image 4"
         className="h-full w-full object-cover"
       />
     </Carousel>
