fffb761c0f185b756b99433467367846c460939f
Maria <mariagracielaesquivelfernandez@gmail.com>
2025-02-22 14:59:51 -0300

    refactor(banner): make component dynamic by accepting props

--- components/banners.tsx
+++ components/banners.tsx
@@ -1,33 +1,23 @@
 'use client'
 
 import { Carousel } from "@material-tailwind/react";
- 
-export default function Banners() {
+interface CarouselProps {
+  images: string[]
+}
+export default function Banners({ images = [] }: CarouselProps) {
   return (
-     <Carousel className="rounded-xl h-96"
-     loop
-     autoplay
-     autoplayDelay={10000}>
-      <img
-        src="img-slider-1.png"
-        alt="image 1"
-        className="h-full w-full object-cover"
-      />
-      <img
-        src="img-slider-2.png"
-        alt="image 2"
-        className="h-full w-full object-cover"
-      />
-      <img
-        src="img-slider-3.png"
-        alt="image 3"
-        className="h-full w-full object-cover"
-      />
-       <img
-        src="img-slider-4.png"
-        alt="image 4"
-        className="h-full w-full object-cover"
-      />
+    <Carousel className="rounded-xl h-126"
+      loop
+      autoplay
+      autoplayDelay={10000}>
+      {images.map((image) => (
+        <img
+          src={image}
+          alt="image"
+          className="h-full w-full object-cover"
+        />
+
+      ))}
     </Carousel>
   );
 }
