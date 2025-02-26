d3f8cd1f08d66ab39dc00c4d51d686a1cfe7e1b4
user.name <gissel.gonzalez@fiuni.edu.py>
2025-02-26 09:51:01 -0300

    dummy data para subir compoennetes a dev

--- components/petCard/cardText.tsx
+++ components/petCard/cardText.tsx
@@ -1,21 +1,59 @@
 import Link from "next/link";
-import React from "react";
+import React, { useMemo } from "react";
 import PostsTags from "./tags";
 
 
+interface props {
+    pet: any,
+    post: any,
+    className?: string,
+}
+
+interface postProps {
+    title: string,
+    description: string,
+    image: string,
+    author: string,
+    tags: string[],
+    postType: string
+}
 
 
+const dummyData = {
+    postType: "adoption",
+    title: "Titulo del card",
+    author: "Vet. Ana Sosa",
+    description: "Encontramos este gatito en un basurero, necesita un hogar amoroso y responsable"
+}
 
-const CardText = (post) => {
+const tags = {
+    tag1: ["race", "mestizo"],
+    tag2: ["age", "2 años"],
+    tag3: ["gender", "female"]
+}
+
+
+const CardText = ({ pet, post, className = "" }: props) => {
+    /*
+        const data = useMemo(() => {
+            return {
+                title: post.title,
+                description: post.description,
+                list: post.type==="adoption" ? post.items : pet.items
+            }
+        }, [pet, post])
+    */
     return (
         <div className="px-2 py-2 flex flex-col bg-white rounded-lg card-text">
             <div className="flex flex-col gap-1">
-                <p className="text-md font-semibold max-h-7 truncate text-ellipsis">{post.title}Title too large for this card causing overflow</p>
-                <div className="flex flex-wrap max-h-16 overflow-hidden">
-                <PostsTags postType="adoption" iconType="race" data="2 años"></PostsTags>
+                <p className="text-md font-semibold max-h-7 truncate text-ellipsis">{dummyData.title}</p>
+                <div className="flex flex-wrap max-h-16 overflow-hidden gap-1">
+                    {Object.values(tags).map(([iconType, data], index) => (
+                        <PostsTags key={index} postType={dummyData.postType} iconType={iconType} data={data} />
+                    ))}
                 </div>
-                <p className="text-xs text-blue-gray-700">{post.author}Vet Ptaricia Sosa</p>
-                <p className="text-sm h-16 overflow-clip text-ellipsis">{post.description}Description Bigotes fue rescatado de la placita, le encontramos en una caja cuando era un bebé pequenhito que no podia comer solo y tomaba biberon</p>
+                <p className="text-xs text-blue-gray-700">{dummyData.author}</p>
+                <p className="text-sm h-16 overflow-clip text-ellipsis">{dummyData.description} </p>
             </div>
         </div>
     );
--- components/petCard/petCard.tsx
+++ components/petCard/petCard.tsx
@@ -4,12 +4,37 @@ import CardImage from "@components/petCard/cardImage";
 import CardText from "./cardText";
 import FavoriteButton from "../buttons/FavoriteButton";
 
-export default function PetCard({ className = "", pet, post }) {
+//Defini estos tipos para que el componente no tenga errores, esto debera cambiar en el futuro cuando el endpoint que conecta
+//posts con pets este implementado
+
+type Pet = {
+    id: string;
+    name: string;
+    species: string;
+    age: number;
+    img_url: string
+};
+
+type Post = {
+    id: string;
+    content: string;
+    date: string;
+    img_url: string
+};
+
+type PetCardProps = {
+    pet: Pet;
+    post: Post;
+    className?: string
+};
+
+
+export default function PetCard({pet, post, className }: PetCardProps) {
     return (
         <div className={clsx("w-64 rounded-xl overflow-hidden bg-white drop-shadow-md flex flex-col relative", className)}>
             <FavoriteButton className="absolute top-2 right-2 z-10" />
-            <CardImage image="defaultcardimg.jpg"/>
-            <CardText />
+            <CardImage/>
+            <CardText pet={pet} post={post} />
         </div>
     );
 }
