fe6ec303d5ca7d8bafb9497a6dba071451533cbf
GermanAquino <german.aquino2019@fiuni.edu.py>
2025-03-02 17:32:09 -0300

    Adición de titulo y detalles al selector

--- app/(pages)/(content)/adoption/page.tsx
+++ app/(pages)/(content)/adoption/page.tsx
@@ -5,6 +5,8 @@ import { useState } from "react";
 import Banners from '@components/banners'
 import PetCard from '@components/petCard/petCard'
 
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+
 const ciudades = ["Encarnación", "Asunción", "Luque", "Fernando Zona Sur"];
 const mascotas = ["Todos", "Conejo", "Perro", "Gato"];
 const edades = ["0-1 años", "1-3 años", "3-6 años", "6+ años"];
@@ -13,51 +15,51 @@ const posts = [
     {
         postId: "1",
         title: "Luna, una perrita cariñosa busca hogar",
-        tags: { especie: "Perro", edad: "2 años", tamaño: "Mediano", ciudad: "Madrid" },
+        tags: { especie: "Perro", edad: "2 años", tamaño: "Mediano", ciudad: "Asunción" },
         author: "Refugio Esperanza",
         content: "Luna es una perrita muy dulce y juguetona. Se lleva bien con niños y otras mascotas. Está vacunada y esterilizada, lista para encontrar un hogar amoroso.",
         date: "2025-02-28",
-        imageUrl: "/images/perro_luna.jpg",
+        imageUrl: "defaultcardimg.jpg",
         postType: "adoption",
     },
     {
         postId: "2",
         title: "Gato siamés en busca de una familia",
-        tags: { especie: "Gato", edad: "3 años", color: "Blanco y gris", ciudad: "Barcelona" },
+        tags: { especie: "Gato", edad: "3 años", color: "Blanco y gris", ciudad: "Asunción" },
         author: "Asociación Felina",
         content: "Este precioso gato siamés es muy sociable y cariñoso. Le encanta jugar y dormir en lugares cálidos. Se entrega con chip y vacunas al día.",
         date: "2025-02-27",
-        imageUrl: "/images/gato_siames.jpg",
+        imageUrl: "defaultcardimg.jpg",
         postType: "adoption",
     },
     {
         postId: "3",
         title: "Cachorros de labrador en adopción",
-        tags: { especie: "Perro", edad: "3 meses", tamaño: "Grande", ciudad: "Sevilla" },
+        tags: { especie: "Perro", edad: "3 meses", tamaño: "Grande", ciudad: "Asunción" },
         author: "Huellitas Sin Hogar",
         content: "Tenemos una camada de 5 cachorros de labrador listos para encontrar familia. Son juguetones y están en proceso de vacunación.",
         date: "2025-02-26",
-        imageUrl: "/images/cachorros_labrador.jpg",
+        imageUrl: "defaultcardimg.jpg",
         postType: "adoption",
     },
     {
         postId: "4",
         title: "Conejito enano necesita un nuevo hogar",
-        tags: { especie: "Conejo", edad: "1 año", color: "Marrón", ciudad: "Valencia" },
+        tags: { especie: "Conejo", edad: "1 año", color: "Marrón", ciudad: "Encarnación" },
         author: "Protectora Animal",
         content: "Este pequeño conejo enano es muy tierno y dócil. Necesita un hogar donde pueda correr libremente y recibir mucho amor.",
         date: "2025-02-25",
-        imageUrl: "/images/conejo_enano.jpg",
+        imageUrl: "defaultcardimg.jpg",
         postType: "adoption",
     },
     {
         postId: "5",
         title: "Gata rescatada busca familia responsable",
-        tags: { especie: "Gato", edad: "2 años", color: "Negro", ciudad: "Bilbao" },
+        tags: { especie: "Gato", edad: "2 años", color: "Negro", ciudad: "Encarnación" },
         author: "Amigos Felinos",
         content: "Rescatamos a esta hermosa gatita de la calle. Es muy cariñosa y está esterilizada. Buscamos un hogar donde la cuiden con amor.",
         date: "2025-02-24",
-        imageUrl: "/images/gata_negra.jpg",
+        imageUrl: "defaultcardimg.jpg",
         postType: "adoption",
     }
 ];
@@ -74,31 +76,41 @@ export default function Page() {
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
 
                     {/* Select Ciudad */}
-                    <CustomSelect
-                        label="Selecciona tu ciudad"
-                        options={ciudades}
-                        selected={selectedCiudad}
-                        setSelected={setSelectedCiudad}
-                    />
+                    <div className="flex flex-col">
+                        <label className="text-sm font-medium text-gray-700 mb-1">Ciudad</label>
+                        <CustomSelect
+                            label="Selecciona tu ciudad"
+                            options={ciudades}
+                            selected={selectedCiudad}
+                            setSelected={setSelectedCiudad}
+                        />
+                    </div>
 
                     {/* Select Mascota */}
-                    <CustomSelect
-                        label="Tipo de mascota"
-                        options={mascotas}
-                        selected={selectedMascota}
-                        setSelected={setSelectedMascota}
-                    />
+                    <div className="flex flex-col">
+                        <label className="text-sm font-medium text-gray-700 mb-1">Tipo de Mascota</label>
+                        <CustomSelect
+                            label="Tipo de mascota"
+                            options={mascotas}
+                            selected={selectedMascota}
+                            setSelected={setSelectedMascota}
+                        />
+                    </div>
 
                     {/* Select Edad */}
-                    <CustomSelect
-                        label="Edad"
-                        options={edades}
-                        selected={selectedEdad}
-                        setSelected={setSelectedEdad}
-                    />
+                    <div className="flex flex-col">
+                        <label className="text-sm font-medium text-gray-700 mb-1">Edad</label>
+                        <CustomSelect
+                            label="Edad"
+                            options={edades}
+                            selected={selectedEdad}
+                            setSelected={setSelectedEdad}
+                        />
+                    </div>
 
                 </div>
             </div>
+
 
             <section>
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-12 px-12 py-4">
@@ -125,8 +137,9 @@ function CustomSelect({
     return (
         <Listbox value={selected} onChange={setSelected}>
             <div className="relative">
-                <Listbox.Button className="w-full bg-white border border-gray-300 rounded-md px-4 py-2 text-left focus:ring-2 focus:ring-blue-500">
+                <Listbox.Button className="w-full flex items-center justify-between px-4 py-2 border border-gray-300 rounded-lg bg-white shadow-sm focus:outline-none">
                     {selected || label}
+                    <ChevronDownIcon className="w-5 h-5 text-gray-500" />
                 </Listbox.Button>
                 <Listbox.Options className="absolute mt-1 w-full bg-white border border-gray-300 rounded-md shadow-lg z-50">
                     {options.map((option, index) => (
