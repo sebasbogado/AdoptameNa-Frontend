e3b62fe6b6752da5ae65ae5ac4d75b096b9f8f9c
sebasbogado <101952377+sebasbogado@users.noreply.github.com>
2025-03-10 01:48:53 -0300

    Se creó la carpeta about us y los archivos page.tsx para la realizacion de la pagina sobre nosotros.

---
+++ app/(pages)/(content)/about-us/page.tsx
@@ -0,0 +1,14 @@
+import Navbar from "@/components/navbar";
+import AboutUs from "@/app/(pages)/(content)/about-us/about-us";
+import Footer from "@/components/footer";
+
+const AboutPage = () => {
+  return (
+    <>
+      <AboutUs />
+      <Footer />
+    </>
+  );
+};
+
+export default AboutPage;
