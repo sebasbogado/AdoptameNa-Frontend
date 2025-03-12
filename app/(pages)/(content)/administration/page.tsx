b9c4c5bd2e13b92152a14ef86638de07489b8658
WMartinez93 <willian.martinez@fiuni.edu.py>
2025-03-12 08:33:40 -0300

    Se agrego un nuevo navbar, se agrego el footer y se oculto parte del navbar principal en la pagina de administracion.

--- app/(pages)/(content)/administration/page.tsx
+++ app/(pages)/(content)/administration/page.tsx
@@ -1,7 +1,16 @@
+import Footer from "@/components/footer";
+import NavbarAdmin from "@/components/navbar-admin";
+
 export default function Page() {
-    return (
+    return (<>
+        <div className="flex flex-col items-center justify-center">
+            <NavbarAdmin/>
+        </div>
         <div>
             Administration
+            <Footer/>
         </div>
+        </>
+
     )
 }
