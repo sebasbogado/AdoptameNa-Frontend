7ad9de5ee0dff4d1d9ea76175288d62d780bd7ad
Maria <mariagracielaesquivelfernandez@gmail.com>
2025-03-12 14:58:49 -0300

    Refactor input and textarea fields in detail-form component

--- components/profile/detail-form.tsx
+++ components/profile/detail-form.tsx
@@ -18,19 +18,23 @@ export const Detail = ({ posts, userProfile, isDisable, setUserProfile }: InputP
 
   return (
     <div className="relative p-8 left-1/3 transform -translate-x-1/2 bg-white shadow-lg rounded-xl p-5 font-roboto z-40 p-6 bg-white shadow-lg rounded-lg mt-[-50px] w-[55vw]">
-      <form>
+          <form>
         <input
           type="text"
           disabled={isDisable}
           value={userProfile?.fullName ?? ""} // Vincula al estado de `userProfile`
-          className="text-5xl font-black bg-transparent border-none focus:outline-none w-full"
+          className={`text-5xl font-black bg-transparent border-2 ${
+            !isDisable ? "border-blue-500 focus:border-blue-700" : "border-transparent"
+          } focus:outline-none w-full`}
           onChange={(e) => handleInputChange('fullName', e.target.value)} // Cambiar valor en `modifiedProfileData`
         />
         <p className="text-foreground text-gray-700 mt-4 text-3xl">{`${posts.length} Publicaciones`}</p>
         <textarea
           disabled={isDisable}
           value={userProfile?.description ?? ""} // Vincula al estado de `userProfile`
-          className="mt-2 text-foreground text-gray-700 mt-8 text-3xl bg-transparent border-none focus:outline-none w-full resize-none"
+          className={`mt-2 text-foreground text-gray-700 mt-8 text-3xl bg-transparent border-2 ${
+            !isDisable ? "border-blue-900 focus:border-blue-700" : "border-transparent"
+          } focus:outline-none w-full resize-none`}
           onChange={(e) => handleInputChange('description', e.target.value)} // Cambiar valor en `modifiedProfileData`
         />
       </form>
