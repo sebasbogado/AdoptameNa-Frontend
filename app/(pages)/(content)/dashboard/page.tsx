4d7574ec559822ef64e8cbee786fe87dfc8afc18
Ivan F <jf561428@gmail.com>
2025-03-11 23:36:35 -0300

    Refactor import paths to solve building problem and update .gitignore to include .vscode

--- app/(pages)/(content)/dashboard/page.tsx
+++ app/(pages)/(content)/dashboard/page.tsx
@@ -1,13 +1,13 @@
 'use client'
 
-import Banners from '@components/banners'
+import Banners from '@/components/banners'
 
 import Footer from '@/components/footer'
 import { useEffect, useState } from 'react'
 import { getPosts } from '@/utils/posts.http'
 import { Post } from '@/types/post'
 
-import Section from '@/components/section'
+import { Section } from '@/components/section'
 
 type FetchContentDataParams = {
     setPosts: React.Dispatch<React.SetStateAction<Post[]>>;
@@ -16,7 +16,7 @@ type FetchContentDataParams = {
 };
 
 
-const fetchContentData = async ({setPosts, setLoading, setError}: FetchContentDataParams) => { 
+const fetchContentData = async ({ setPosts, setLoading, setError }: FetchContentDataParams) => {
 
     try {
         const postData = await getPosts({});
@@ -39,7 +39,7 @@ export default function Page() {
     }, []);
 
 
-  
+
 
     const bannerImages = ["banner1.png", "banner2.png", "banner3.png", "banner4.png"]
     return (
@@ -48,22 +48,22 @@ export default function Page() {
             {/* Sección de Adopción 
                 Los postTypeId seran numero magicos mientras se cambia en el back
             */}
-            <Section title = 'En adopcion'  path='adoption' postType="adoption" items={posts} loading = {loading} error = {error}></Section>
-               
+            <Section title='En adopcion' path='adoption' postType="adoption" items={posts} loading={loading} error={error}></Section>
+
 
             {/* Sección de Desaparecidos */}
-            <Section title = 'Extraviados'  path='missing' postType="missing" items={posts} loading = {loading} error = {error}></Section>
+            <Section title='Extraviados' path='missing' postType="missing" items={posts} loading={loading} error={error}></Section>
 
-            
+
             {/* Sección de Voluntariado */}
-            <Section title = 'Voluntariado'  path='volunteering' postType="volunteering" items={posts} loading = {loading} error = {error}></Section>
+            <Section title='Voluntariado' path='volunteering' postType="volunteering" items={posts} loading={loading} error={error}></Section>
 
 
             {/* Sección de Blogs */}
-            <Section title = 'Blog'  path='blog' postType="blog" items={posts} loading = {loading} error = {error}></Section>
+            <Section title='Blog' path='blog' postType="blog" items={posts} loading={loading} error={error}></Section>
 
             {/* Marketplace */}
-            <Section title = 'Tienda' path='marketplace' postType="marketplace" items={posts} loading = {loading} error = {error}></Section>
+            <Section title='Tienda' path='marketplace' postType="marketplace" items={posts} loading={loading} error={error}></Section>
             <Footer />
         </div>
     )
--- components/Section.tsx
+++ components/Section.tsx
@@ -1,41 +1,40 @@
-import { ChevronRightIcon } from "@heroicons/react/24/outline";
 import PetCard from "@/components/petCard/pet-card";
 import Title from "./title";
 
-import {titleText } from "../types/title"
+import { titleText } from "../types/title"
 import { Post } from "@/types/post";
 import { Pet } from "@/types/pet";
 
 interface SectionProps {
-  title: string;
-  postType: keyof typeof titleText;
-  postTypeId?: number; 
-  path: string;
-  items: Post[] | Pet[]; 
-  loading: boolean;
-  error: string | null;
+    title: string;
+    postType: keyof typeof titleText;
+    postTypeId?: number;
+    path: string;
+    items: Post[] | Pet[];
+    loading: boolean;
+    error: string | null;
 }
 const isPost = (item: Post | Pet): item is Post => {
-  return (item as Post).postTypeName !== undefined;
+    return (item as Post).postTypeName !== undefined;
 };
-export default function Section({ title, postType, path, items, loading, error }: SectionProps) {
-  return (
-    <div className="mt-12 ml-6">
-        <Title title={title} postType={postType} path={path}></Title>
+export function Section({ title, postType, path, items, loading, error }: SectionProps) {
+    return (
+        <div className="mt-12 ml-6">
+            <Title title={title} postType={postType} path={path}></Title>
 
-      {loading ? (
-        <p className="text-gray-500">Cargando {title.toLowerCase()}...</p>
-      ) : error ? (
-        <p className="text-red-500">{error}</p>
-      ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-5 gap-8 mt-2 p-2">
-         {items
-           .filter(isPost)
-            .filter((post) => post.postTypeName == postType)
-            .slice(0, 5)
-            .map((post) => <PetCard key={post.id} post={post} />)}
+            {loading ? (
+                <p className="text-gray-500">Cargando {title.toLowerCase()}...</p>
+            ) : error ? (
+                <p className="text-red-500">{error}</p>
+            ) : (
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-5 gap-8 mt-2 p-2">
+                    {items
+                        .filter(isPost)
+                        .filter((post) => post.postTypeName == postType)
+                        .slice(0, 5)
+                        .map((post) => <PetCard key={post.id} post={post} />)}
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 }
