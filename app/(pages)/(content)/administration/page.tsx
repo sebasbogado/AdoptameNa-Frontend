0ee4e4d7ff6735ef1e30f7d523b6eeff08b04a5b
WMartinez93 <willian.martinez@fiuni.edu.py>
2025-03-13 18:08:36 -0300

    Se implemento redireccion por rol.

--- app/(pages)/(content)/administration/page.tsx
+++ app/(pages)/(content)/administration/page.tsx
@@ -1,7 +1,29 @@
+"use client"
+import Loading from "@/app/loading";
 import Footer from "@/components/footer";
 import NavbarAdmin from "@/components/navbar-admin";
+import { User as UserType } from "@/types/auth";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
-export default function Page() {
+export default function Page({ currentUser }: { currentUser: UserType }) {
+
+
+    const router = useRouter();
+    
+    useEffect(() => {
+        // Verifica si el usuario no tiene rol de 'admin'
+        if (!currentUser || currentUser.role !== "admin") {
+          // Si el rol no es 'admin', redirige a la página de home
+          router.push("/dashboard") 
+        }
+      }, [currentUser, router]); // El efecto se ejecuta cuando el usuario o el router cambian
+    
+      if (!currentUser || currentUser.role !== "admin") {
+        // Mostrar una interfaz de carga mientras se procesa la redirección
+        <Loading/>
+      }
+      
     return (<>
         <div className="flex flex-col items-center justify-center">
             <NavbarAdmin/>
