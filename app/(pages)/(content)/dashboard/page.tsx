1023d674b0ead80ff540698b9b87ce146a1023a4
sebasbogado <101952377+sebasbogado@users.noreply.github.com>
2025-03-17 12:16:34 -0300

    Se agrega un + al boton de publicacion

--- app/(pages)/(content)/dashboard/page.tsx
+++ app/(pages)/(content)/dashboard/page.tsx
@@ -115,7 +115,7 @@ export default function Page() {
                 <div className="fixed bottom-5 right-5">
                     <button className="group flex items-center gap-2 bg-[#FFAE34] text-white px-4 py-2 rounded-full shadow-lg hover:px-6 transition-all duration-500">
                         <span className="text-lg transition-all duration-500 group-hover:hidden">+</span>
-                        <span className="hidden group-hover:inline transition-all duration-500">Crear publicación</span>
+                        <span className="hidden group-hover:inline transition-all duration-500">+ Crear publicación</span>
                     </button>
                 </div>
             </Link>
