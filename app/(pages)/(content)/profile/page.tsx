1b15e702fb5c20a93575ade7239364c29789d811
Maria <mariagracielaesquivelfernandez@gmail.com>
2025-03-18 02:05:57 -0300

    Added pet image filtering and banner image condition in ProfilePage component

--- app/(pages)/(content)/profile/page.tsx
+++ app/(pages)/(content)/profile/page.tsx
@@ -208,6 +208,7 @@ export default function ProfilePage() {
         return true;
     };
 
+    const petImages = pets.map(pet => pet.urlPhoto).filter(url => url !== null);
 
     if (authLoading || loading) {
         return <Loading />;
@@ -216,7 +217,7 @@ export default function ProfilePage() {
     return (
         <div className="w-full font-roboto">
             {/* Banner */}
-            <Banners images={userProfile?.bannerImages || ['/profile/slider/img-slider-1.png']} />
+            <Banners images={petImages.length > 0 ? petImages : ['/logo.png']} />
 
             {/* User Info */}
             <Detail
