143310f8bb22312edb995f3ac68dcfe79dc5bcaf
Ivan F <jf561428@gmail.com>
2025-03-19 15:31:53 -0300

    feat: rename getPets function to getPetsByUserId for clarity and update references

--- utils/pets.http.ts
+++ utils/pets.http.ts
@@ -1,10 +1,45 @@
+import { Pet } from "@/types/pet";
 import axios from "axios";
 
 const API_URL = `${process.env.NEXT_PUBLIC_BASE_API_URL}/pets`;
 
-export const getPets = async (id: string) => {
+export const getPetsByUserId = async (id: string) => {
   try {
     const response = await axios.get(`${API_URL}/${id}/user`, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    return response.data;
+  } catch (error: any) {
+    if (error.response && error.response.status === 404) {
+      throw new Error("No encontrada");
+    }
+    throw new Error(error.message || "Error al obtener Pets");
+  }
+};
+
+export const getPet = async (id: string): Promise<Pet> => {
+  try {
+    const response = await axios.get(`${API_URL}/${id}`, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    return response.data;
+  } catch (error: any) {
+    if (error.response && error.response.status === 404) {
+      throw new Error("No encontrada");
+    }
+    throw new Error(error.message || "Error al obtener Pets");
+  }
+};
+
+export const getPets = async (): Promise<Pet[]> => {
+  try {
+    const response = await axios.get(API_URL, {
       headers: {
         "Content-Type": "application/json",
       },
