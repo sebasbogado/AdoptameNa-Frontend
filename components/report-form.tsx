fc7cca32ccb136b835aa87c8903fcd8c9e169b65
jorgef29 <jorgefigueredo2001@gmail.com>
2025-03-18 23:09:59 -0300

    feature: post report

--- components/report-form.tsx
+++ components/report-form.tsx
@@ -4,11 +4,25 @@ import Button from '@/components/buttons/button';
 import { getReportReasons } from '@/utils/report-reasons.http';
 import { useAuth } from '@/contexts/authContext';
 import { ReportReason } from '@/types/report-reason';
+import { Report } from '@/types/report';
+import { createReport } from '@/utils/reports.http';
 
-const ReportForm = () => {
+interface ReportFormProps {
+  handleClose: () => void;
+}
+const ReportForm: React.FC<ReportFormProps> = ({ handleClose }) => {
   const [reportReasons, setReportReasons] = useState([]);
-  const [report, setReport] = useState(null);
+  const [report, setReport] = useState<Report>({
+    id: 0,
+    idUser: 7,
+    idPost: 2,
+    idReportReason: 0,
+    description: new Date().toISOString(),
+    reportDate: "",
+    status: "",
+  });
   const { authToken } = useAuth();
+
   useEffect(() => {
     const fetchReportReasons = async () => {
       try {
@@ -25,22 +39,55 @@ const ReportForm = () => {
   useEffect(() => {
     console.log(reportReasons);
   }, [reportReasons]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setReport((prevReport) => ({
+      ...prevReport,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async () => {
+    if (!authToken) {
+      console.error("Error: No hay token de autenticación.");
+      return;
+    }
+
+    if (!report.idReportReason || report.description.trim() === "") {
+      console.error("Error: Debes seleccionar un motivo y escribir una descripción.");
+      return;
+    }
+
+    try {
+      const response = await createReport(authToken, report);
+      console.log("Reporte enviado con éxito:", response);
+    } catch (error) {
+      console.error("Error al enviar el reporte:", error);
+    }
+  };
+
   return (
     <>
       <form className='mt-3'>
-        <label htmlFor="" className='text-base font-normal block'>Razon del reporte</label>
-        <select name="" id="" className='w-full border rounded-lg p-2'>
-          {reportReasons.map((reason: ReportReason) => (
-            <option key={reason.id} value={reason.id}>{reason.description}</option>
-          ))}
-        </select>
-        <label htmlFor="">Descripción</label>
-        <Textarea placeholder="Escribe una descripción" />
-        <div className='flex justify-end'>
-          <Button variant="danger" size="md"> Confirmar Reporte </Button>
-
+        <div>
+          <label htmlFor="idReportReason" className='text-base font-normal block'>Motivo del reporte</label>
+          <select className='w-full border rounded-lg p-2' name="idReportReason" value={report.idReportReason} onChange={handleChange}>
+            <option value="">Seleccion motivo del reporte</option>
+            {reportReasons.map((reason: ReportReason) => (
+              <option key={reason.id} value={reason.id}>{reason.description}</option>
+            ))}
+          </select>
+        </div>
+        <div className='mt-4'>
+          <label htmlFor="description" className='text-base font-normal block'>Descripción</label>
+          <Textarea name="description" placeholder="Escribe una descripción" value={report.description} onChange={handleChange} />
         </div>
       </form>
+      <div className='flex justify-end pt-4 space-x-2'>
+        <Button variant='secondary' size='md' onClick={handleClose}>Cancelar </Button>
+        <Button variant="danger" size="md" onClick={handleSubmit}> Confirmar Reporte </Button>
+      </div>
     </>
   );
 }
