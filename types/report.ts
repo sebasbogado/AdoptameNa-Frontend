2e32aeb95367edba49e6b84f4e2cae8b55791b2c
Maria <mariagracielaesquivelfernandez@gmail.com>
2025-03-20 04:51:08 -0300

    Added getPostReports function to utils/posts.http.ts

---
+++ types/report.ts
@@ -0,0 +1,10 @@
+
+export interface Report{
+    id: number;
+    idUser: number;
+    idPost: number;
+    idReportReason: number;
+    description: string;
+    reportDate: string;
+    status: string
+}
--- utils/posts.http.ts
+++ utils/posts.http.ts
@@ -37,3 +37,19 @@ export const getPost = async (id: string): Promise<Post> => {
     throw new Error(error.message || "Error al obtener Post");
   }
 };
+export const getPostReports = async (queryParams?: any)=>{
+  try{
+    const response = await axios.get(`${API_URL}/reported`, {
+      params: queryParams,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      });
+      return response.data;
+      }catch(error: any){
+        if(error.response && error.response.status === 404){
+          throw new Error("No encontrada");
+        }
+        throw new Error(error.message || "Error al obtener Posts reportados");
+  }
+}
