d8398066b916584eb146ac3b4cfe7eceb184b879
Ivan F <jf561428@gmail.com>
2025-03-20 23:20:53 -0300

    Refactor animal fetching and post handling; update PetCard to support post type

--- types/post.ts
+++ types/post.ts
@@ -1,14 +1,15 @@
+import { PostType } from "./post-type";
+
 export interface Post {
-    id: number;
-    idUser: number;
-    title: string;
-    content: string;
-    idPostType: number;
-    locationCoordinates: string;
-    contactNumber: string;
-    status: string;
-    sharedCounter: number;
-    publicationDate: string;
-    urlPhoto : string;
-    postTypeName: string;
+  id: number;
+  idUser: number;
+  title: string;
+  content: string;
+  locationCoordinates: string;
+  contactNumber: string;
+  status: string;
+  sharedCounter: number;
+  publicationDate: string;
+  urlPhoto: string;
+  postType: PostType;
 }
