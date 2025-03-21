7c2412f911f5a7ab99a28487571fc887c5cd7ed7
Ivan F <jf561428@gmail.com>
2025-03-20 23:24:26 -0300

    Refactor authentication handling and update admin navbar links

--- components/navbar-admin.tsx
+++ components/navbar-admin.tsx
@@ -4,32 +4,32 @@ import { usePathname } from "next/navigation";
 
 // Lista de enlaces de navegación
 const navbarAdminItems = [
-    { name: "Administrar Publicaciones", path: "" },
-    {name: "Publicaciones Eliminadas", path: ""},
-    { name: "Reportes", path: "" },
-    { name: "Configuraciones", path: "" },
+    { name: "Administrar Publicaciones", path: "/administration/posts" },
+    { name: "Publicaciones Eliminadas", path: "/administration/deleted-posts" },
+    { name: "Reportes", path: "/administration/reports" },
+    { name: "Configuraciones", path: "/administration/settings" },
     { name: "Animales y Mascotas", path: "/administration/configuration" },
     { name: "Razas", path: "/administration/breeds" }
 ];
 
-export default function NavbarAdmin(){
+export default function NavbarAdmin() {
 
     const pathname = usePathname(); // Obtener la ruta actual
 
     return (
 
         <div className="hidden items-center gap-12 md:flex">
-                    {navbarAdminItems.map(({ name, path }) => (
-                        <Link
-                            key={path}
-                            href={path}
-                            className={`text-lg font-bold hover:text-purple-600 ${pathname === path ? "text-purple-600" : "text-black"
-                                }`}
-                        >
-                            {name}
-                        </Link>
-                    ))}
-                </div>
+            {navbarAdminItems.map(({ name, path }) => (
+                <Link
+                    key={path}
+                    href={path}
+                    className={`text-lg font-bold hover:text-purple-600 ${pathname === path ? "text-purple-600" : "text-black"
+                        }`}
+                >
+                    {name}
+                </Link>
+            ))}
+        </div>
 
     )
 }
