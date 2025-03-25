014e7c3a39f8aa0a479b742dc1fff19aa9aad9b0
jorgef29 <jorgefigueredo2001@gmail.com>
2025-03-25 12:13:43 -0300

    fix submit

--- components/form-animal.tsx
+++ components/form-animal.tsx
@@ -2,10 +2,10 @@ import React, { useState } from 'react';
 import { Input } from '@/components/ui/input';
 import Button from '@/components/buttons/button';
 import { Animal } from '@/types/animal';
-import { Alert } from '@material-tailwind/react';
 import { animalSchema } from '@/validations/animal-schema';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
+
 interface FormAnimalsProps {
   onCreate: (newAnimal: any) => void;
   onDelete: (event: React.FormEvent) => void;
@@ -15,36 +15,36 @@ interface FormAnimalsProps {
 const FormAnimals: React.FC<FormAnimalsProps> = ({ onCreate, onDelete, animalData }) => {
   const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm({
     resolver: zodResolver(animalSchema),
+    defaultValues: {
+      id: animalData.id || undefined,
+      name: animalData.name,
+    },
   });
 
   const onSubmit = (data: any) => {
     onCreate(data);
   };
-
   return (
     <form className="mt-5" onSubmit={handleSubmit(onSubmit)}>
-      {errors.name && (
-        <Alert color="red" className="py-2">
-          {errors.name?.message}
-        </Alert>
-      )}
-
       <div className="mb-2">
         <label className="block mb-1">Nombre </label>
         <Input
           placeholder="Ejemplo: perro"
           maxLength={100}
+          className={`w-full border p-2 rounded ${errors.name ? 'border-red-500 focus:outline-none' : 'border-gray-300'}`}
           {...register("name")}
         />
+        {errors.name && (
+          <p className='text-red-500 text-xs mt-1'>{errors.name.message}</p>)}
       </div>
 
       <div className="flex justify-end items-center mt-6 w-full">
         <div className="flex gap-4">
-          <Button variant={animalData.id === 0 ? "secondary" : "danger"} onClick={onDelete}>
+          <Button variant={animalData.id === 0 ? "secondary" : "danger"} type="button" onClick={onDelete}>
             {animalData.id === 0 ? "Cancelar" : "Eliminar"}
           </Button>
 
-          <Button variant='primary' disabled={isSubmitting}>
+          <Button variant='primary' type='submit' disabled={isSubmitting}>
             {isSubmitting ? "Guardando..." : "Guardar"}
           </Button>
         </div>
