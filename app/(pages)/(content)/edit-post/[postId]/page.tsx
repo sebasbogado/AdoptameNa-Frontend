40fb9e4ab0d4deb4137d77c613c86a525e682d2c
sebasbogado <101952377+sebasbogado@users.noreply.github.com>
2025-03-26 08:54:28 -0300

    Se agrega la utilización de zod para la validación en editar una publicación.

--- app/(pages)/(content)/edit-post/[postId]/page.tsx
+++ app/(pages)/(content)/edit-post/[postId]/page.tsx
@@ -2,7 +2,7 @@
 
 import Loading from "@/app/loading";
 import { useAuth } from "@/contexts/authContext";
-import { Post } from "@/types/post";
+import { Post, UpdatePost } from "@/types/post";
 import { PostType } from "@/types/post-type";
 import { getPostsType } from "@/utils/post-type.http";
 import { deletePost, getPost, updatePost } from "@/utils/posts.http";
@@ -14,14 +14,9 @@ import NotFound from "@/app/not-found";
 import Banners from "@/components/banners";
 import { MapProps } from "@/types/map-props";
 import dynamic from "next/dynamic";
-
-interface FormErrors {
-    postType?: string;  // Cambiado de idPostType a postType
-    title?: string;
-    content?: string;
-    locationCoordinates?: string;
-    contactNumber?: string;
-}
+import { PostFormValues, postSchema } from "@/validations/post-schema";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 const MapWithNoSSR = dynamic<MapProps>(
     () => import('@/components/ui/map'),
@@ -36,59 +31,42 @@ export default function Page() {
     const [loading, setLoading] = useState<boolean>(true);
     const [postError, setPostError] = useState<string | null>(null);
     const router = useRouter();
-
     const [successMessage, setSuccessMessage] = useState<string | null>(null);
     const [error, setError] = useState<string | null>(null);
-    const [formErrors, setFormErrors] = useState<FormErrors>({});
-    const [formData, setFormData] = useState<Partial<Post>>({
-        idUser: user ? Number(user.id) : 0,
+    const {
+        register,
+        handleSubmit: zodHandleSubmit,  // Renombramos el handleSubmit de useForm
+        setValue,
+        formState: { errors, isSubmitting }
+    } = useForm<PostFormValues>({
+        resolver: zodResolver(postSchema),
+        defaultValues: {
+            idPostType: 0,
+            title: "",
+            content: "",
+            locationCoordinates: [0, 0],
+            contactNumber: "",
+            urlPhoto: ""
+        }
+    });
+    const [formData, setFormData] = useState<PostFormValues>({
+        idPostType: 0,
         title: "",
         content: "",
-        postType: {
-            id: 0,
-            name: "",
-            description: "",
-        },
-        locationCoordinates: "",
+        locationCoordinates: [0, 0], // Array de coordenadas
         contactNumber: "",
+        status: "activo", // Valor estático
         urlPhoto: "",
-        status: "",
-        sharedCounter: 0,
-        publicationDate: undefined,
     });
-
-    {/* Validación de los campos */ }
-    const validateForm = (payload: any): FormErrors => {
-        const errors: FormErrors = {};
-
-        if (!payload.postType?.id || payload.postType.id === 0) {  // Cambiado a postType.id
-            errors.postType = "Seleccione un tipo de publicación";
-        }
-        if (!payload.title?.trim()) {
-            errors.title = "El título es requerido";
-        } else if (payload.title.length < 3) {
-            errors.title = "El título debe tener al menos 3 caracteres";
-        }
-        if (!payload.content?.trim()) {
-            errors.content = "La descripción es requerida";
-        } else if (payload.content.length < 10) {
-            errors.content = "La descripción debe tener al menos 10 caracteres";
-        }
-        if (!payload.locationCoordinates?.trim()) {
-            errors.locationCoordinates = "La ubicación es requerida";
-        }
-        if (!payload.contactNumber?.trim()) {
-            errors.contactNumber = "El número de contacto es requerido";
-        } else if (!/^\+?\d{9,15}$/.test(payload.contactNumber)) {
-            errors.contactNumber = "Número inválido (9-15 dígitos)";
-        }
-
-        return errors;
-    };
 
     const [isEditModalOpen, setIsEditModalOpen] = useState(false);
     const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
     const [position, setPosition] = useState<[number, number] | null>(null);
+
+    const handlePositionChange = (newPosition: [number, number]) => {
+        setPosition(newPosition); // Actualiza el estado local
+        setValue("locationCoordinates", newPosition); // Actualiza el formulario
+    };
 
     useEffect(() => {
         if (!authLoading && !authToken) {
@@ -113,15 +91,18 @@ export default function Page() {
 
             try {
                 const postData = await getPost(String(postId));
-                console.log("dato response", postData.locationCoordinates);
+                if (postData) {
+                    setPost(postData);
 
-                if (postData.locationCoordinates) {
                     const [lat, lng] = postData.locationCoordinates.split(',').map(Number);
-                    console.log("mapa actualizado", lat, lng);
                     setPosition([lat, lng]);
+                    setValue("idPostType", postData.postType?.id || 0);
+                    setValue("title", postData.title || "");
+                    setValue("content", postData.content || "");
+                    setValue("contactNumber", postData.contactNumber || "");
+                    setValue("locationCoordinates", [lat, lng]);
+                    setValue("urlPhoto", postData.urlPhoto || "");
                 }
-                setFormData(postData);
-                setPost(postData);
             } catch (err) {
                 console.error("Error al cargar posts:", err);
                 setPostError("No se pudieron cargar las publicaciones.");
@@ -146,53 +127,24 @@ export default function Page() {
     if (loading) return Loading();
     if (postError !== null) return NotFound();
 
-    const handleChange = (
-        e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>
-    ) => {
-        const { name, value } = e.target;
-        if (name === "postType") {  // Cambiado de idPostType a postType
-            const selectedPostType = postTypes.find(type => type.id === Number(value));
-            setFormData((prev) => ({
-                ...prev,
-                postType: selectedPostType || { id: 0, name: "", description: "" }
-            }));
-        } else {
-            setFormData((prev) => ({ ...prev, [name]: value }));
-        }
-        if (formErrors[name as keyof FormErrors]) {
-            setFormErrors((prev) => ({
-                ...prev,
-                [name]: undefined,
-            }));
-        }
-    };
-
-    const handleSubmit = async (e: React.FormEvent) => {
-        e.preventDefault();
+    const openConfirmationModalEdit = (data: PostFormValues) => {
+        setFormData(data); // Guardamos los datos validados
         setIsEditModalOpen(true);
     };
 
-    const confirmEdit = async () => {
+    const handleSubmit = async () => {
         setIsEditModalOpen(false);
         if (!post || !authToken) return;
 
-        const payload = {
+        const updatedFormData = {
             ...formData,
-            idPostType: Number(formData.postType?.id),  // Extraemos el ID del objeto postType
             idUser: user ? Number(user.id) : 0,
             locationCoordinates: position ? `${position[0]}, ${position[1]}` : ""
         };
 
-        const validationErrors = validateForm(payload);
-        if (Object.keys(validationErrors).length > 0) {
-            setFormErrors(validationErrors);
-            setLoading(false);
-            return;
-        }
-
         setLoading(true);
         try {
-            await updatePost(String(post.id), payload as Post, authToken);
+            await updatePost(String(post.id), updatedFormData as UpdatePost, authToken);
             setSuccessMessage('¡Publicación actualizada con éxito!');
             setTimeout(() => router.push(`/posts/${post.id}`), 1500);
         } catch (error) {
@@ -203,11 +155,11 @@ export default function Page() {
         }
     };
 
-    const handleDelete = () => {
+    const openConfirmationModalDelete = () => {
         setIsDeleteModalOpen(true);
     };
 
-    const confirmDelete = async () => {
+    const handleDelete = async () => {
         setIsDeleteModalOpen(false);
         if (!post?.id || !authToken) {
             console.error('Falta el ID de la publicación o el token');
@@ -237,7 +189,7 @@ export default function Page() {
         router.push(`/posts/${post.id}`);
     };
 
-    const arrayImages = formData?.urlPhoto ? [formData.urlPhoto] : [];
+    const arrayImages = formData?.urlPhoto ? [formData.urlPhoto] : ['../logo.png'];
     return (
         <div className="max-w-2xl mx-auto p-6 bg-white shadow-lg rounded-lg">
             <Banners images={arrayImages} />
@@ -247,87 +199,56 @@ export default function Page() {
                 <div className="mb-4 p-2 bg-green-100 text-green-700 rounded">{successMessage}</div>
             )}
 
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={zodHandleSubmit(openConfirmationModalEdit)}>
                 {/* Tipo de publicación */}
-                <label className="block text-sm font-medium">
-                    Tipo de publicación <span className="text-red-500">*</span>
-                </label>
                 <select
-                    name="postType"  // Cambiado de idPostType a postType
-                    value={formData.postType?.id || ""}  // Usamos el id del objeto postType
-                    onChange={handleChange}
-                    className={`w-full p-2 border rounded mb-4 ${formErrors.postType ? 'border-red-500' : ''}`}
+                    {...register("idPostType", { valueAsNumber: true })}
+                    className={`w-full p-2 border rounded mb-4 ${errors.idPostType ? 'border-red-500' : ''}`}
                 >
-                    <option value="">Seleccione un tipo</option>
+                    <option value={0}>Seleccione un tipo</option>
                     {postTypes.map((type) => (
                         <option key={type.id} value={type.id}>{type.name}</option>
                     ))}
                 </select>
-                {formErrors.postType && (
-                    <p className="text-red-500 text-sm mb-4">{formErrors.postType}</p>
-                )}
+                {errors.idPostType && <p className="text-red-500 text-sm">{errors.idPostType.message}</p>}
 
                 {/* Título */}
-                <label className="block text-sm font-medium">
-                    Título <span className="text-red-500">*</span>
-                </label>
+                <label className="block text-sm font-medium">Título <span className="text-red-500">*</span></label>
                 <input
                     type="text"
-                    name="title"
-                    value={formData.title}
-                    onChange={handleChange}
-                    className={`w-full p-2 border rounded mb-4 ${formErrors.title ? 'border-red-500' : ''}`}
+                    {...register("title")}
+                    className={`w-full p-2 border rounded mb-4 ${errors.title ? 'border-red-500' : ''}`}
                 />
-                {formErrors.title && (
-                    <p className="text-red-500 text-sm mb-4">{formErrors.title}</p>
-                )}
+                {errors.title && <p className="text-red-500 text-sm">{errors.title.message}</p>}
 
                 {/* Descripción */}
-                <label className="block text-sm font-medium">
-                    Descripción <span className="text-red-500">*</span>
-                </label>
+                <label className="block text-sm font-medium">Descripción <span className="text-red-500">*</span></label>
                 <textarea
-                    name="content"
-                    value={formData.content}
-                    onChange={handleChange}
-                    className={`w-full p-2 border rounded mb-4 ${formErrors.content ? 'border-red-500' : ''}`}
+                    {...register("content")}
+                    className={`w-full p-2 border rounded mb-4 ${errors.content ? 'border-red-500' : ''}`}
                 />
-                {formErrors.content && (
-                    <p className="text-red-500 text-sm mb-4">{formErrors.content}</p>
-                )}
+                {errors.content && <p className="text-red-500 text-sm">{errors.content.message}</p>}
 
                 {/* Contacto */}
-                <label className="block text-sm font-medium">
-                    Número de contacto <span className="text-red-500">*</span>
-                </label>
+                <label className="block text-sm font-medium">Número de contacto <span className="text-red-500">*</span></label>
                 <input
                     type="text"
-                    name="contactNumber"
-                    value={formData.contactNumber}
-                    onChange={handleChange}
-                    className={`w-full p-2 border rounded mb-4 ${formErrors.contactNumber ? 'border-red-500' : ''}`}
+                    {...register("contactNumber")}
+                    className={`w-full p-2 border rounded mb-4 ${errors.contactNumber ? 'border-red-500' : ''}`}
                 />
-                {formErrors.contactNumber && (
-                    <p className="text-red-500 text-sm mb-4">{formErrors.contactNumber}</p>
-                )}
+                {errors.contactNumber && <p className="text-red-500 text-sm">{errors.contactNumber.message}</p>}
 
                 {/* Mapa */}
-                <div
-                    className={`h-full relative transition-opacity duration-300 ${isEditModalOpen || isDeleteModalOpen ? "pointer-events-none opacity-50" : ""}`}
-                >
-                    <MapWithNoSSR position={position} setPosition={setPosition} />
-                </div>
-                {formErrors.locationCoordinates && (
-                    <p className="text-red-500 text-sm mb-4">{formErrors.locationCoordinates}</p>
-                )}
+                <MapWithNoSSR position={position} setPosition={handlePositionChange} />
+                {errors.locationCoordinates && <p className="text-red-500">{errors.locationCoordinates.message}</p>}
 
                 <div className="flex justify-between items-center mt-6 gap-10">
-                    <Button
+                <Button
                         type="button"
                         variant="danger"
                         size="md"
                         className="rounded hover:bg-red-700"
-                        onClick={handleDelete}
+                        onClick={openConfirmationModalDelete}
                         disabled={loading}
                     >
                         {loading ? 'Eliminando...' : 'Eliminar publicación'}
@@ -337,7 +258,6 @@ export default function Page() {
                         <Button
                             type="button"
                             variant="tertiary"
-                            size="md"
                             className="border rounded text-gray-700 hover:bg-gray-100"
                             onClick={handleCancel}
                             disabled={loading}
@@ -345,11 +265,9 @@ export default function Page() {
                             Cancelar
                         </Button>
                         <Button
-                            type="button"
+                            type="submit"
                             variant="cta"
-                            size="md"
                             className="rounded hover:bg-purple-700"
-                            onClick={confirmEdit}
                             disabled={loading}
                         >
                             {loading ? "Editando..." : "Confirmar cambios"}
@@ -366,7 +284,7 @@ export default function Page() {
                     textConfirm="Confirmar cambios"
                     confirmVariant="cta"
                     onClose={closeModal}
-                    onConfirm={confirmEdit}
+                    onConfirm={handleSubmit}
                 />}
 
             {isDeleteModalOpen &&
@@ -377,7 +295,7 @@ export default function Page() {
                     textConfirm="Eliminar"
                     confirmVariant="danger"
                     onClose={closeModal}
-                    onConfirm={confirmDelete}
+                    onConfirm={handleDelete}
                 />}
         </div>
     );
