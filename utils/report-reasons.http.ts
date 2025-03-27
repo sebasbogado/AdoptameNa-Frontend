35227c137a0f43bd7b9f10b8f9f40b541bf348ce
jorgef29 <jorgefigueredo2001@gmail.com>
2025-03-27 11:12:20 -0300

    add create report reason

--- utils/report-reasons.http.ts
+++ utils/report-reasons.http.ts
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { ReportReason } from "@/types/report-reason";
+
 const API_URL = `${process.env.NEXT_PUBLIC_BASE_API_URL}/reportReasons`;
 
 export const getReportReasons = async (queryParams?:any) => {
@@ -17,3 +19,20 @@ export const getReportReasons = async (queryParams?:any) => {
     throw new Error(error.message || "Error al obtener Report Reasons");
   }
 };
+
+export const createReportReason = async (newReportReason: ReportReason, token:string) => {
+  try {
+    const response = await axios.post(API_URL, newReportReason, {
+      headers: {
+        "Authorization": `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
+    return response.data;
+  } catch (error: any) {
+    if (error.response && error.response.status === 404) {
+      throw new Error("No encontrada");
+    }
+    throw new Error(error.message || "Error al crear Report Reason");
+  }
+}
