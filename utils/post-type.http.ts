a3b804e24b94222ec3f3a629da41e332800097cf
WMartinez93 <willian.martinez@fiuni.edu.py>
2025-04-12 18:05:19 -0300

    Se deshizo cambio para el correcto funcionamiento de la pagina

--- utils/post-type.http.ts
+++ utils/post-type.http.ts
@@ -12,7 +12,7 @@ export const getPostsType = async (queryParams?: any) => {
       },
     });
 
-    return response.data.data;
+    return response.data;
   } catch (error: any) {
     if (error.response && error.response.status === 404) {
       throw new Error("No encontrada");
