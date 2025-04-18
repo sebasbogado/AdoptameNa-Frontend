a30aa8afa5ca1bd9670517f68144bcaf2e93422d
WMartinez93 <willian.martinez@fiuni.edu.py>
2025-04-18 14:07:23 -0300

    Se agrego filtros a la pagina, la paginacion funciona bien a la hora de mostrar los datos, pero no con los filtros.

--- app/(pages)/(content)/marketplace/page.tsx
+++ app/(pages)/(content)/marketplace/page.tsx
@@ -1,58 +1,102 @@
 'use client'
 
 import Banners from "@/components/banners";
+import LabeledSelect from "@/components/labeled-selected";
 import Pagination from "@/components/pagination";
 import PetCard from "@/components/petCard/pet-card";
 import { usePagination } from "@/hooks/use-pagination";
 import { Post } from "@/types/post";
+import { Product } from "@/types/product";
+import { ProductCategory } from "@/types/product-category";
 import { getPosts } from '@/utils/posts.http';
+import { getProductCategories } from "@/utils/product-category.http";
+import { getProducts } from "@/utils/products.http";
 import { useEffect, useState } from "react";
 
 export default function Page() {
 
     const bannerImages = ["banner1.png", "banner2.png", "banner3.png", "banner4.png"];
-    const [postsMarket, setPostsMarket] = useState<Post[]>([]);
+    const [ selectedCategory, setSelectedCategory ] = useState<string | null>(null);
+    const [selectedCategoryId, setSelectedCategoryId] = useState<number | null>(null);
+    const [selectedPrice, setSelectedPrice] = useState<number | null>(null);
+    const [selectedCondition, setSelectedCondition] = useState<string | null>(null);
+
+    const [categories, setCategories] = useState<ProductCategory[]>([]);
+    
 
     useEffect(() => {
-        const fetchData = async () => {
-          try {
-            const response: any = await getPosts();
-            const marketDataFiltered = response.data.filter(
-              (item: { postType: { name: string; }; }) => item.postType.name === "Marketplace"
-            );
-            setPostsMarket(marketDataFiltered);
-          } catch (err: any) {
-            console.log(err.message);
-          } finally {
-            setLoading(false);
-          }
-        };
-
-        fetchData();
-    }, []);
+        if (selectedCategory) {
+          const found = categories.find(cat => cat.name === selectedCategory);
+          setSelectedCategoryId(found ? found.id : null);
+        } else {
+          setSelectedCategoryId(null);
+        }
+      }, [selectedCategory, categories]);
+      
+      useEffect(() => {
+        const found = categories.find(cat => cat.name === selectedCategory);
+        setSelectedCategoryId(found ? found.id : null);
+    }, [selectedCategory, categories]);
 
     const pageSize = 3;
         const {
-            data: posts,
+            data: products,
             loading,
             error,
             currentPage,
             totalPages,
             handlePageChange,
-        } = usePagination<Post>({
-            fetchFunction: async (page, size) => {
-                        return await getPosts({ page, size });
-                    },
+        } = usePagination<Product>({
+            fetchFunction: (page, size) => getProducts( selectedCategoryId || undefined, selectedCondition || undefined, selectedPrice || undefined, page, size),
                     initialPage: 1,
                     initialPageSize: pageSize
                 });
+
+    useEffect(() => {
+        const fetchCategories = async () => {
+            try {
+                const data = await getProductCategories();
+                setCategories(data.data); 
+                        
+            } catch (error) {
+                console.error("Error al obtener categorías:", error);
+            }
+        };
+                
+        fetchCategories();
+    }, []); 
 
     return (
         <div className='flex flex-col gap-5'>
             <Banners images={bannerImages} />
             <div className="w-full max-w-4xl mx-auto p-4">
                 <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
-                    {/**Aqui va a ir el filtro. */}
+                    <LabeledSelect
+                        label="Categorias"
+                        options={categories.map((category) => category.name)}
+                        selected={selectedCategory}
+                        setSelected={setSelectedCategory}                    
+                    />
+
+                    <LabeledSelect
+                        label="Precio"
+                        options={
+                            products.length === 0 
+                                ? ["No hay precios disponibles"] 
+                                : [...new Set(products.map(p => p.price))].sort((a, b) => a - b)
+                        }
+                        selected={selectedPrice}
+                        setSelected={setSelectedPrice}                    
+                    />
+
+                    <LabeledSelect
+                        label="Condicion"
+                        options={["NUEVO", "USADO"]}
+                        selected={selectedCondition}
+                        setSelected={setSelectedCondition}                    
+                    />
+
+
                 </div>
             </div>
 
@@ -61,10 +105,10 @@ export default function Page() {
                     
                     {loading ? (
                         <p className="text-center col-span-full">Cargando datos...</p>
-                    ) : posts.length === 0 ? (
+                    ) : products.length === 0 ? (
                         <p className="text-center col-span-full">No se han encontrado resultados</p>
                     ) : (
-                        posts.map((item) => (
+                        products.map((item) => (
                             <PetCard key={item.id} post={item} />
                         ))
                     )}
