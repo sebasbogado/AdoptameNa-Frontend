e893c5e391e7a09f0a48b904e78febfcebf39da4
user.name <gissel.gonzalez@fiuni.edu.py>
2025-04-18 18:05:29 -0300

    Cambie tag img por Image de next

--- components/petCard/card-image.tsx
+++ components/petCard/card-image.tsx
@@ -1,4 +1,5 @@
 import { Media } from "@/types/media";
+import Image from "next/image";
 import React from "react";
 
 interface CardImageProps {
@@ -10,10 +11,13 @@ const notFoundSrc = "/logo.png";
 const CardImage: React.FC<CardImageProps> = ({ media }) => {
     return (
         <div className="h-36 rounded-lg overflow-hidden">
-            <img
+            <Image
                 className="w-full h-auto object-cover"
                 src={media?.url || notFoundSrc}
                 alt="Imagen de mascota"
+                width={500}
+                height={500}
+                unoptimized
             />
         </div>
     );
