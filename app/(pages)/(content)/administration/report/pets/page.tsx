f377ffc5add1b002e2b99d8e77fb549bec602769
jorgef29 <jorgefigueredo2001@gmail.com>
2025-04-18 18:13:49 -0300

    component for report list

--- app/(pages)/(content)/administration/report/pets/page.tsx
+++ app/(pages)/(content)/administration/report/pets/page.tsx
@@ -9,30 +9,12 @@ import { useEffect } from "react";
 import { usePagination } from '@/hooks/use-pagination';
 import Pagination from "@/components/pagination";
 import { Loader2 } from 'lucide-react';
-
+import ReportListPage from "@/components/administration/report/report-list-page";
 
 export default function Page() {
   const { authToken, user, loading: authLoading } = useAuth();
   const router = useRouter();
-  const pageSize = 10;
-
-  const {
-    data: pets,
-    loading,
-    error,
-    currentPage,
-    totalPages,
-    handlePageChange
-  } = usePagination<Pet>({
-    fetchFunction: async (page, size) => {
-      if (!authToken) {
-        throw new Error("No se ha encontrado el token de autenticación");
-      }
-      return await getReportedPets(authToken, { page, size });
-    },
-    initialPage: 1,
-    initialPageSize: pageSize
-  });
+  const pageSize = 20;
 
   useEffect(() => {
     if (!authLoading && !authToken) {
@@ -45,33 +27,15 @@ export default function Page() {
 
   return (
     <div className="p-6">
-      <SectionAdmin title="Aprobar o rechazar denuncias">Bloquear un reporte indica que es correcto y se eliminará la publicación, mantener un reporte indica que el reporte no es correcto y la publicación seguirá activa</SectionAdmin>
-      {error && (
-        <div className="bg-red-100 text-red-700 p-4 rounded-md w-full max-w-md">
-          {error.message || 'Error al cargar las publicaciones reportadas'}
-        </div>
-      )}
-      {loading ? (
-        <div className="flex justify-center items-center">
-          <Loader2 className="h-10 w-10 animate-spin text-purple-500" />
-        </div>) : (
-        pets.length !== 0 ? (
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-5 gap-8 mt-2 p-2">
-            {pets.map((pet, index) => (
-              <CardReport key={index} post={pet} isPost={false} />
-            ))}
-          </div>
-        ) : (
-          <div className="text-center p-10 bg-gray-50 rounded-lg w-full max-w-md">
-            <p className="text-gray-600">No hay mascotas reportadas</p>
-          </div>
-        )
-      )}
-      <Pagination
-        totalPages={totalPages}
-        currentPage={currentPage}
-        onPageChange={handlePageChange}
-        size='md'
+      <ReportListPage
+        fetchFunction={async (page, size) => {
+          if (!authToken) {
+            throw new Error("No se ha encontrado el token de autenticación");
+          }
+          return await getReportedPets(authToken, { page, size });
+        }
+        }
+        pageSize={pageSize}
       />
     </div>
   )
--- app/(pages)/(content)/administration/report/posts/page.tsx
+++ app/(pages)/(content)/administration/report/posts/page.tsx
@@ -1,38 +1,15 @@
 'use client'
-import CardReport from "@/components/administration/report/card-button";
-import SectionAdmin from "@/components/administration/section";
 import { useAuth } from "@/contexts/auth-context";
-import { Post } from "@/types/post";
 import { getReportedPosts } from "@/utils/report-client";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
-import { usePagination } from '@/hooks/use-pagination';
-import Pagination from "@/components/pagination";
-import { Loader2 } from 'lucide-react';
-
+import ReportListPage from "@/components/administration/report/report-list-page";
 
 export default function Page() {
 	const { authToken, user, loading: authLoading } = useAuth();
 	const router = useRouter();
 	const pageSize = 10;
 
-	const {
-		data: posts,
-		loading,
-		error,
-		currentPage,
-		totalPages,
-		handlePageChange
-	} = usePagination<Post>({
-		fetchFunction: async (page, size) => {
-			if (!authToken) {
-				throw new Error("No se ha encontrado el token de autenticación");
-			}
-			return await getReportedPosts(authToken, { page, size });
-		},
-		initialPage: 1,
-		initialPageSize: pageSize
-	});
 
 	useEffect(() => {
 		if (!authLoading && !authToken) {
@@ -45,34 +22,13 @@ export default function Page() {
 
 	return (
 		<div className="p-6">
-			<SectionAdmin title="Aprobar o rechazar denuncias">Bloquear un reporte indica que es correcto y se eliminará la publicación, mantener un reporte indica que el reporte no es correcto y la publicación seguirá activa</SectionAdmin>
-			{error && (
-				<div className="bg-red-100 text-red-700 p-4 rounded-md w-full max-w-md">
-					{error.message || 'Error al cargar las publicaciones reportadas'}
-				</div>
-			)}
-			{loading ? (
-				<div className="flex justify-center items-center">
-					<Loader2 className="h-10 w-10 animate-spin text-purple-500" />
-				</div>) : (
-				posts.length !== 0 ? (
-					<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-5 gap-8 mt-2 p-2">
-						{posts.map((post, index) => (
-							<CardReport key={index} post={post} />
-						))}
-					</div>
-				) : (
-					<div className="text-center p-10 bg-gray-50 rounded-lg w-full max-w-md">
-						<p className="text-gray-600">No hay publicaciones reportadas</p>
-					</div>
-				)
-			)}
-			<Pagination
-				totalPages={totalPages}
-				currentPage={currentPage}
-				onPageChange={handlePageChange}
-				size='md'
+			<ReportListPage
+				fetchFunction={(page, size) => {
+					if(!authToken) return Promise.reject(new Error("No se ha encontrado el token de autenticación"));
+					return getReportedPosts(authToken, { page, size })}
+				}
 			/>
 		</div>
+		
 	)
 }
