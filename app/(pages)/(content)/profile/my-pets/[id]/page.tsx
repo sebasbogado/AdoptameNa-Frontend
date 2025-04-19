4cff9c02faa49cd9c77f6e3a4a2144f85c97f840
sebasbogado <101952377+sebasbogado@users.noreply.github.com>
2025-04-18 22:14:12 -0300

    Se agrega los filtros para la página de my pets

--- app/(pages)/(content)/profile/my-pets/[id]/page.tsx
+++ app/(pages)/(content)/profile/my-pets/[id]/page.tsx
@@ -13,6 +13,8 @@ import { getPetsByUserId } from '@/utils/pets.http';
 import { error } from 'console';
 import Pagination from '@/components/pagination';
 import { usePagination } from '@/hooks/use-pagination';
+import { Animal } from '@/types/animal';
+import { getAnimals } from '@/utils/animals.http';
 
 
 export default function MyPostsPage() {
@@ -20,11 +22,14 @@ export default function MyPostsPage() {
     const mascotas = ["Todos", "Conejo", "Perro", "Gato"];
     const edades = ["0-1 años", "1-3 años", "3-6 años", "6+ años"];
 
-    const {id} = useParams();
+    const { id } = useParams();
 
     const [selectedCiudad, setSelectedCiudad] = useState<string | null>(null);
     const [selectedMascota, setSelectedMascota] = useState<string | null>(null);
+    const [selectedMascotaId, setSelectedMascotaId] = useState<number | null>(null);
     const [selectedEdad, setSelectedEdad] = useState<string | null>(null);
+
+    const [animals, setAnimals] = useState<Animal[]>([]);
 
     const pageSize = 3;
     const {
@@ -33,19 +38,67 @@ export default function MyPostsPage() {
         error,
         currentPage,
         totalPages,
+        updateFilters,
         handlePageChange,
     } = usePagination<Pet>({
-        fetchFunction: (page, size) => getPetsByUserId(id as string, page, size),
+        fetchFunction: (page, size, filters) =>
+            getPetsByUserId({
+                page,
+                size,
+                userId: Number(id),
+                animalId: selectedMascotaId || undefined,
+                age: selectedEdad ? parseInt(selectedEdad.split("-")[0]) : undefined,
+            }),
         initialPage: 1,
         initialPageSize: pageSize,
     });
 
+    const cleanFilters = (filters: Record<string, any>) => {
+        return Object.fromEntries(
+            Object.entries(filters).filter(([_, v]) => v !== null && v !== undefined)
+        );
+    };
+
+    useEffect(() => {
+        const fetchAnimal = async () => {
+            try {
+                const animals = await getAnimals();
+                setAnimals(animals.data);
+            } catch (error) {
+                console.error("Error al obtener el tipo de mascota:", error);
+            }
+        };
+
+        fetchAnimal();
+    }, []);
+
+    useEffect(() => {
+        if (selectedMascota && !selectedMascota.includes("Todos")) {
+            const found = animals.find(a => a.name === selectedMascota);
+            setSelectedMascotaId(found ? found.id : null);
+        } else {
+            setSelectedMascotaId(null);
+        }
+    }, [selectedMascota, animals]);
+
+    useEffect(() => {
+    
+
+        const filteredData = {
+            animalId: selectedMascotaId,
+            age: selectedEdad ? parseInt(selectedEdad.split("-")[0]) : undefined,
+            city: selectedCiudad,
+        };
+    
+        const cleanedFilters = cleanFilters(filteredData);
+        updateFilters(cleanedFilters);
+    }, [selectedMascotaId, selectedEdad, selectedCiudad, updateFilters]);
 
     if (loading) {
         return Loading();
     }
     const bannerImages = ["/banner1.png", "/banner2.png", "/banner3.png", "/banner4.png"]
-    if(loading) return <Loading />
+    if (loading) return <Loading />
 
     return (
         <div className='flex flex-col gap-5'>
@@ -66,7 +119,7 @@ export default function MyPostsPage() {
                     {/* Select Mascota */}
                     <LabeledSelect
                         label="Mascota"
-                        options={mascotas}
+                        options={["Todos", ...animals.map((animal) => animal.name)]}
                         selected={selectedMascota}
                         setSelected={setSelectedMascota}
                     />
@@ -84,25 +137,25 @@ export default function MyPostsPage() {
 
             <section>
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-12 px-12 py-4">
-                    
-                    {error? <p className="text-center col-span-full">Hubo un error al cargar las mascotas</p> : 
-                    pets.length > 0 ? (
-                        pets.map((item) => (
-                            <PetCard key={item.id} post={item} />
-                        ))
-                    ) : (
-                        <p className="text-center col-span-full">Cargando mascotas...</p>
-                    )}
+
+                    {error ? <p className="text-center col-span-full">Hubo un error al cargar las mascotas</p> :
+                        pets.length > 0 ? (
+                            pets.map((item) => (
+                                <PetCard key={item.id} post={item} />
+                            ))
+                        ) : (
+                            <p className="text-center col-span-full">Cargando mascotas...</p>
+                        )}
 
                 </div>
             </section>
 
-                <Pagination
-                    totalPages={totalPages}
-                    currentPage={currentPage}
-                    onPageChange={handlePageChange}
-                    size="md"
-                />
+            <Pagination
+                totalPages={totalPages}
+                currentPage={currentPage}
+                onPageChange={handlePageChange}
+                size="md"
+            />
         </div>
     )
 }
