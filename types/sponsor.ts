c375f383b51a98f226036b2395f3f7c5cba420d8
Jose Ferreira <jf561428@gmail.com>
2025-04-22 23:35:59 -0400

    Revert "Revert "Crear pantalla de administracion auspiciantes""

--- types/sponsor.ts
+++ types/sponsor.ts
@@ -1,9 +1,26 @@
 export type Sponsor = {
   id: number;
+  idUser: number;
   organizationName: string;
+  fullName: string;
+  reason: string;
+  contact: string;
+  logoId: number | null; // Hacer opcional/nullable
+  bannerId: number | null; // Hacer opcional/nullable
+  isActive: boolean;
+};
+
+// Nuevo tipo para sponsors activos
+export type ActiveSponsor = {
+  id: number;
   logoUrl: string;
-  bannerUrl: string;
+};
+
+// Tipo para la creación de sponsors
+export type CreateSponsorRequest = {
+  contact: string;
+  reason: string;
   logoId: number;
-  bannerId: number;
+  bannerId?: number;
 };
 
