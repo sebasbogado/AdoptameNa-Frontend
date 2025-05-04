82d302a964260980896643c4e52211baba294a00
jorgef29 <jorgefigueredo2001@gmail.com>
2025-05-04 14:00:45 -0300

    feature: format number function

--- utils/Utils.ts
+++ utils/Utils.ts
@@ -4,3 +4,7 @@ export const capitalizeFirstLetter = (str: string) => {
   if (!str) return "";
   return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 }
+
+export const formatNumber = (value: number) => {
+  return value.toLocaleString('es-PY');
+}
