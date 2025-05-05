72eba5f15e4cb5188d75109a826294100f3f98ae
sebasbogado <101952377+sebasbogado@users.noreply.github.com>
2025-05-05 11:52:19 -0300

    Se modifica para utilizar filtros, se agregó el filtrado por estado

--- app/(pages)/(content)/administration/deleted-posts/pets-publications/page.tsx
+++ app/(pages)/(content)/administration/deleted-posts/pets-publications/page.tsx
@@ -1,13 +1,45 @@
 'use client'
 import { useAuth } from "@/contexts/auth-context";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import DeletedListPage from "@/components/administration/deleted/deleted-list-page";
-import { getDeletedPosts } from "@/utils/posts.http";
+import { usePagination } from "@/hooks/use-pagination";
+import LabeledSelect from "@/components/labeled-selected";
+import ResetFiltersButton from "@/components/reset-filters-button";
+import { getDeletedPets } from "@/utils/pets.http";
+import { Pet } from "@/types/pet";
 
 export default function Page() {
     const { authToken, user, loading: authLoading } = useAuth();
     const router = useRouter();
+
+    const [selectedPetStatus, setSelectedPetStatus] = useState<string | null>(null);
+    const [petStatusOptions, setPetStatusOptions] = useState<string[]>([]);
+    const [allPetStatusMap, setAllPetStatusMap] = useState<Record<string, number>>({});
+
+    const [pageSize, setPageSize] = useState<number>();
+    const [postError, setPostError] = useState<string | null>(null);
+
+    const {
+        data: pets,
+        loading,
+        currentPage,
+        totalPages,
+        handlePageChange,
+        updateFilters
+    } = usePagination<Pet>({
+        fetchFunction: async (page, size, filters) => {
+            if (!authToken) throw new Error("Authentication token is missing");
+            return await getDeletedPets(authToken, {
+                page,
+                size,
+                userId: filters?.userId ?? undefined,
+                petStatusId: filters?.petStatusId ?? undefined
+            });
+        },
+        initialPage: 1,
+        initialPageSize: pageSize
+    });
 
     useEffect(() => {
         if (!authLoading && !authToken) {
@@ -18,15 +50,67 @@ export default function Page() {
 
     }, [authToken, authLoading, router]);
 
+    useEffect(() => {
+        const fetchDeletedData = async () => {
+            try {
+                const [petResponse] = await Promise.all([
+                    authToken ? getDeletedPets(authToken) : Promise.reject(new Error("Authentication token is missing"))
+                ]);
+
+                const petStatusMap: Record<string, number> = {};
+                petResponse.data.forEach(pet => {
+                    if (pet.petStatus.name) {
+                        petStatusMap[pet.petStatus.name] = pet.petStatus.id;
+                    }
+                });
+
+                const uniquePetStatus = Object.keys(petStatusMap).sort();
+
+                setPetStatusOptions(uniquePetStatus);
+                setAllPetStatusMap(petStatusMap);
+                setPageSize(petResponse.pagination.size);
+            } catch (err) {
+                setPostError("Error al obtener las publicaciones")
+            }
+        };
+
+        fetchDeletedData();
+    }, []);
+
+    const resetFilters = () => {
+        setSelectedPetStatus(null);
+        updateFilters({});
+    };
+
+    useEffect(() => {
+        const petStatusId = selectedPetStatus && selectedPetStatus !== "Todos" ? allPetStatusMap[selectedPetStatus] : undefined;
+
+        updateFilters({ petStatusId });
+        handlePageChange(1);
+    }, [selectedPetStatus]);
+
     return (
         <div className="p-6">
+            <div className="w-full max-w-6xl mx-auto p-4">
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+                    <LabeledSelect
+                        label="Estado"
+                        options={["Todos", ...petStatusOptions]}
+                        selected={selectedPetStatus}
+                        setSelected={setSelectedPetStatus}
+                    />
+
+                    <ResetFiltersButton onClick={resetFilters} />
+                </div>
+            </div>
             <DeletedListPage
-                fetchFunction={(page, size) => {
-                    if(!authToken) return Promise.reject(new Error("No se ha encontrado el token de autenticación"));
-                    return getDeletedPosts(authToken, { page, size })}
-                }
+                posts={pets}
+                loading={loading}
+                error={postError}
+                currentPage={currentPage}
+                totalPages={totalPages}
+                handlePageChange={handlePageChange}
             />
         </div>
-        
     )
 }
