e58c4aaf9c82438c698d7ec34bcd7137088d844e
sebasbogado <101952377+sebasbogado@users.noreply.github.com>
2025-05-05 16:09:14 -0300

    Se modifica el cardDeleted para que realice la función de restaurar una publicación eliminada

--- components/administration/deleted/card-deleted.tsx
+++ components/administration/deleted/card-deleted.tsx
@@ -2,56 +2,41 @@
 import React from "react";
 import clsx from "clsx";
 import PetCard from "@/components/petCard/pet-card";
-import { useRouter } from "next/navigation";
-import Link from "next/link";
+import { useRouter } from "next/navigation";;
 import Button from "@/components/buttons/button";
-import { ArchiveRestore, CheckIcon, EyeIcon, XIcon } from "lucide-react";
+import { ArchiveRestore } from "lucide-react";
 
 //Defini estos tipos para que el componente no tenga errores, esto debera cambiar en el futuro cuando el endpoint que conecta
 //posts con pets este implementado
+type ItemType = 'post' | 'pet' | 'product';
 
 type PetCardProps = {
-    post: any;
-    className?: string
-    isDeletedPage?: boolean
-    handleAprove?: () => void;
-    handleDesaprove?: () => void;
-    isPost?: boolean
+    item: any;
+    itemType: string;
+    className?: string;
+    onRestore: (id: string | number, type: string) => void;
 };
 
-export default function CardDeleted({ post, className, isDeletedPage, handleAprove, handleDesaprove, isPost = true }: PetCardProps) {
-    const router = useRouter()
+export default function CardDeleted({ item, itemType, className, onRestore }: PetCardProps) {
+
+    const handleRestoreClick = () => {
+        // Llama a onRestore pasando tanto el ID como el TIPO
+        onRestore(item.id, itemType);
+    };
+
     return (
         <div className={clsx("w-64 rounded-xl overflow-hidden bg-white drop-shadow-md flex flex-col relative", className)}>
-            {isDeletedPage ?
-                <div onClick={(e) => e.stopPropagation()}>
-                    <PetCard post={post} isPost={isPost} />
-                </div> :
-                <PetCard post={post} isPost={isPost} />}
-            {isDeletedPage ?
-                <div className="m-4 flex gap-2 justify-center">
-
-                    <Button size="sm" onClick={handleAprove} className="flex items-center justify-center">
-                        <CheckIcon className="w-3 h-3 mr-2 text-white" strokeWidth={4} />
-                        Mantener
-                    </Button>
-                    <Button variant="danger" size="sm" onClick={handleDesaprove} className="flex items-center justify-center">
-                        <XIcon className="w-3 h-3  mr-2 text-white" strokeWidth={4} />
-                        Bloquear
-                    </Button>
-                </div>
-                :
-                <div className="m-4 flex justify-center">
-                    <Link href={isPost ? `/administration/report/posts/${post.id}` : `/administration/report/pets/${post.id}`} >
-                        <Button size="sm" className="flex items-center justify-center">
-                            <ArchiveRestore className="w-5 h-5 mr-2 text-white" strokeWidth={3} />
-                            Restaurar
-                        </Button>
-                    </Link>
-                </div>
-            }
-
-
+            <PetCard post={item} />
+            <div className="m-4 flex justify-center">
+                <Button
+                    onClick={handleRestoreClick}
+                    size="sm"
+                    className="flex items-center justify-center"
+                >
+                    <ArchiveRestore className="w-5 h-5 mr-2 text-white" strokeWidth={3} />
+                    Restaurar
+                </Button>
+            </div>
         </div>
     );
 }
