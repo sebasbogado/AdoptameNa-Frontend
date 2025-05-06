4dd701573330973aa92f9f849493da6685d3849f
sebasbogado <sebastian.bogado2019@fiuni.edu.py>
2025-05-06 14:31:28 -0300

    Se agrega la vista para la pantalla de comentarios reportados

---
+++ app/(pages)/(content)/administration/report/comments/page.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import { useAuth } from "@/contexts/auth-context";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import ReportListPage from "@/components/administration/report/report-list-page";
+import { getReportedComments } from "@/utils/report-client";
+import { ITEM_TYPE } from "@/types/constants";
+
+export default function Page() {
+  const { authToken, user, loading: authLoading } = useAuth();
+  const router = useRouter();
+  const pageSize = 20;
+
+  useEffect(() => {
+    if (!authLoading && !authToken) {
+      router.push("/auth/login");
+    } else if (user && user.role !== "admin") {
+      router.push("/dashboard");
+    }
+
+  }, [authToken, authLoading, router]);
+
+  return (
+    <div className="p-6">
+      <ReportListPage
+        type={ITEM_TYPE.COMMENT}
+        fetchFunction={async (page, size) => {
+          if (!authToken) {
+            throw new Error("No se ha encontrado el token de autenticación");
+          }
+          return await getReportedComments(authToken, { page, size });
+        }
+        }
+        pageSize={pageSize}
+        isPost={false}
+      />
+    </div>
+  )
+}
