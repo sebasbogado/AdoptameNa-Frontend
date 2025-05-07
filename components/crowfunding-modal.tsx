f121bbe58159c79d3aba3188c79325ecdc93667c
PK <pedrojugador2@hotmail.com>
2025-05-07 00:05:02 -0300

    feat: add crowdfunding schema and type definitions for validation

--- components/crowfunding-modal.tsx
+++ components/crowfunding-modal.tsx
@@ -13,31 +13,8 @@ import {
 import { useAuth } from "@/contexts/auth-context";
 import Button from "@/components/buttons/button";
 import ConfirmationModal from "@/components/confirm-modal";
-
-const crowdfundingSchema = z.object({
-    title: z
-        .string()
-        .min(3, "El título debe tener al menos 3 caracteres.")
-        .max(255, "El título no debe exceder 255 caracteres."),
-    description: z
-        .string()
-        .min(10, "La descripción debe tener al menos 10 caracteres.")
-        .max(1000, "La descripción no debe exceder 1000 caracteres."),
-    goal: z
-        .number()
-        .min(0.01, "La meta debe ser mayor a 0"),
-    durationDays: z
-        .union([z.number().min(1).max(365), z.undefined()])
-        .optional(), // <-- hace que no sea requerido al editar
-});
-
-interface Crowdfunding {
-    id: number;
-    title: string;
-    description: string;
-    durationDays: number;
-    goal: number;
-}
+import { crowdfundingSchema } from "@/validations/crowfunding-schema";
+import { Crowdfunding } from "@/types/crowfunding-type"
 
 interface CrowdfundingModalProps {
     open: boolean;
