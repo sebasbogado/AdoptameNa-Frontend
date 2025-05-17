f5cd0944c74af868ce7a2f05cd61c5a04edb4114
sebasbogado <101952377+sebasbogado@users.noreply.github.com>
2025-05-17 01:09:07 -0300

    Add CrowdfundingCardText and OrganizationTag components for displaying crowdfunding details

---
+++ components/crowdfundingCard/tag.tsx
@@ -0,0 +1,34 @@
+import clsx from "clsx";
+import React from "react";
+
+const tagBaseClass = 'rounded-md px-1 w-fit border flex justify-center items-center p-1'
+import { HandHelpingIcon, StarIcon } from "lucide-react";
+interface TagProps {
+    iconType: string;
+    value: string;
+}
+
+const tagColors: Record<string, string> = {
+    Blog: "text-blog border-blog",
+};
+
+const attributeIcons: Record<string, React.ElementType> = {
+    star: StarIcon,
+    handHelp: HandHelpingIcon,
+};
+
+
+const OrganizationTag: React.FC<TagProps> = ({ iconType, value }) => {
+    const IconComponent = attributeIcons[iconType];
+
+    return (
+        
+        <div className={clsx(tagBaseClass, tagColors['Blog'])}>
+            {IconComponent && <IconComponent className="w-4 h-4 mr-1" />}
+            
+            <span className="text-xs">{value}</span>
+        </div>
+    )
+}
+
+export default OrganizationTag;
