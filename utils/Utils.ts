114672fcf1469dc6a73fe55f9a513d6e3dec4694
sebasbogado <101952377+sebasbogado@users.noreply.github.com>
2025-05-21 15:23:24 -0300

    Add utility functions for gender, vaccination, sterilization, and animal icons

--- utils/Utils.ts
+++ utils/Utils.ts
@@ -8,3 +8,31 @@ export const capitalizeFirstLetter = (str: string) => {
 export const formatNumber = (value: number) => {
   return value.toLocaleString('es-PY');
 }
+
+export const convertGenderToSpanish = (str: string) => {
+  return str === "MALE" ? "Macho" : str === "FEMALE" ? "Hembra" : str;
+}
+
+export const getGenderIcon = (str: string) => {
+  return str === "MALE" ? "male" : str === "FEMALE" ? "female" : str;
+}
+
+export const getVaccinatedIcon = (vaccined: boolean) => {
+  return vaccined ? "vaccinated" : "";
+}
+
+export const getSterilizedIcon = (vaccined: boolean) => {
+  return vaccined ? "sterilized" : "";
+}
+
+export const getAnimalIcon = (str: string) => {
+  return str === "perro" ? "dog" : str === "Perros" ? "dog" : str === "gato" ? "cat" : str === "Gatos" ? "cat" : str === "tortuga" ? "turtle" : str === "conejo" ? "rabbit" : "generic";
+}
+
+export const getConditionIcon = (str: string) => {
+  return str === "USADO" ? "old" : str === "NUEVO" ? "new" : str;
+}
+
+export const getColorAdoptionOrMissing = (str: string) => {
+  return str === "En Adopción" ? "Adopcion" : str === "Perdido" ? "Extraviados" : "Extraviados";
+}
