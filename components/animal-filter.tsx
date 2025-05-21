444d122e8d99bdf3c722398e7147bc4d8e098c49
Jorge Figueredo <jorgefigueredo2001@gmail.com>
2025-05-20 21:14:18 -0300

    add: z-index

--- components/animal-filter.tsx
+++ components/animal-filter.tsx
@@ -27,7 +27,7 @@ const AnimalFilter: React.FC<AnimalFilterProps> = ({ selectedFilter, setSelected
       </button>
 
       {filterOpen && (
-        <div className="absolute right-0 mt-2 w-56 bg-white border border-gray-200 rounded-lg shadow-lg">
+        <div className="absolute right-0 mt-2 w-56 bg-white border border-gray-200 rounded-lg shadow-lg z-30">
           <div
             className="cursor-pointer px-4 py-2 hover:bg-gray-100"
             onClick={() => {
