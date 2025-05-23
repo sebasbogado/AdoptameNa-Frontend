e89038aff64210d94dd9eac72d1cf8fb973bcb2e
WMartinez93 <willian.martinez@fiuni.edu.py>
2025-05-23 17:06:00 -0300

    Test automatizado para ver el detalle de un reporte de mascota que hizo el usuario logueado.

---
+++ cypress/e2e/my-pets-reports.cy.ts
@@ -0,0 +1,29 @@
+describe("Admin Panel - Búsqueda de usuario - TC-ADMIN-01", () => {
+    it("Debería iniciar sesión, ir a mis reportes y ver el detalle del primer card, verificando que se muestre el detalle del mismo.", () => {
+
+    // 1. Login con credenciales válidas de admin
+      cy.visit("/auth/login");
+      cy.get('input[name="email"]').type("rodrigo.maidana2019@fiuni.edu.py", { delay: 100 });
+      cy.get('input[name="password"]').type("Rodrigo29", { delay: 100 });
+      cy.get('button[type="submit"]').click();
+  
+      cy.wait(7000);
+      cy.url().should("include", "/dashboard");
+
+      cy.get("#avatar").should("be.visible").click();
+
+      cy.contains("Mis reportes").click();
+
+      cy.wait(4000);
+
+      cy.url().should("include", "/profile/report");
+
+      cy.get('a[href="/profile/report/pets"]').click();
+
+      cy.wait(4000);
+
+      cy.get('a[href="/profile/report/pets/16"] button').first().contains('Ver Reportes').click();
+    
+      cy.url().should('match', /\/profile\/report\/pets\/\d+$/);
+
+    })})
