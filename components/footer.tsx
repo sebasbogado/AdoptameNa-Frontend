010e5be6c1bcd63d6d5947a7842b9e380e1da298
sebasbogado <101952377+sebasbogado@users.noreply.github.com>
2025-05-26 09:44:50 -0300

    fix: adjust footer layout and image size for better responsiveness

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,11 +5,11 @@ import Link from "next/link";
 const Footer: React.FC = () => {
     return (
         <footer className="p-10 bg-btn-cta text-white relative overflow-clip">
-            <div className="flex justify-between w-1/2">
+            <div className="flex flex-col md:flex-row md:justify-between w-full md:w-3/4 gap-8">
                 {footerSections.map((section) => (
                     <div key={section.title} className="flex flex-col gap-3 text-lg">
                         <h1 className="font-bold">{section.title}</h1>
-                        <ul className="decoration-transparent p-0 flex flex-col gap-1">
+                        <ul className="p-0 flex flex-col gap-1">
                             {section.links.map((link) => (
                                 <li key={link.name}>
                                     <Link href={link.path} className="decoration-transparent">
@@ -25,7 +25,7 @@ const Footer: React.FC = () => {
             <img
                 src="/footer_img.png"
                 alt="Imagen de footer"
-                className="absolute -bottom-3 right-0 w-52 h-auto"
+                className="absolute -bottom-3 right-0 w-32 md:w-52 h-auto"
             />
         </footer>
     );
