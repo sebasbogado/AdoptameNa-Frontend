2e96cbf1e2ebb4dd90751c989795a032a3662a2d
WMartinez93 <willian.martinez@fiuni.edu.py>
2025-05-26 09:37:09 -0300

    Se agrego test automatizado para el contacto con soporte. No se verifica que el clientede correo predeterminado del uruario se haya abierto, por que cypress no puede verificar acciones fuera del navegador.

---
+++ cypress/e2e/contact-to-support.cy.ts
@@ -0,0 +1,16 @@
+describe("Contact to support", () => {
+  it("Deberia iniciar sesion, hacer clich en el boton del footer para contactar al soporte porcorreo.", () => {
+    // 1. Login con credenciales válidas
+    cy.visit("/auth/login");
+    cy.get('input[name="email"]').type("rodrigo.maidana2019@fiuni.edu.py", { delay: 100 });
+    cy.get('input[name="password"]').type("Rodrigo29", { delay: 100 });
+    cy.get('button[type="submit"]').click();
+
+    cy.wait(7000);
+    cy.url().should("include", "/dashboard");
+
+    cy.wait(2000);
+    cy.get('#contact-to-support-link').click();
+
+
+  })})
